722894e11e32f0a394d7706034fa41339caa67de
marceloamaro <marcelo.amaro@aluno.uece.br>
2021-06-11 08:13:47 -0300

    pratica

--- Aula01/pratica_aula01/fetchoneBD.py
+++ Aula01/pratica_aula01/fetchoneBD.py
@@ -3,12 +3,13 @@ conexao = sqlite3.connect("ctmombaça.db")
 
 cursor = conexao.cursor()
 
-
+# comando para fazer a busca na tabela
 cursor.execute("select * from ctmombaça")
 
-
+# fetchone retorna uma tupla com os resultados da consulta
 resultado = cursor.fetchone()
 
+# fstring que captura os dados da tupla através da indexação
 print(f"Nome: {resultado[0]}\nModalidade: {resultado[1]}")
 
 
