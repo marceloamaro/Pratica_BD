6acc35b01bdabdfbcdb8c4d2d37b0987041f0a22
marceloamaro <marcelo.amaro@aluno.uece.br>
2021-06-11 08:12:11 -0300

    pratica

--- Aula01/pratica_aula01/fetchallDB.py
+++ Aula01/pratica_aula01/fetchallDB.py
@@ -3,13 +3,13 @@ conexao = sqlite3.connect("ctmombaça.db")
 
 cursor = conexao.cursor()
 
-
+# comando para fazer a busca na tabela
 cursor.execute("select * from ctmombaça")
 
-
+# fetchone retorna uma tupla com os resultados da consulta
 resultado = cursor.fetchall()
 
-
+# Loop que captura os dados da tupla através da indexação
 for registro in resultado:
     print(f"Nome: {registro[0]}\nModalidade: {registro[1]}")
 
