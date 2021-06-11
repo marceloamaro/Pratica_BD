89c55a895a0a8ad36278d47b940aa6524769c9c7
marceloamaro <marcelo.amaro@aluno.uece.br>
2021-06-11 07:50:21 -0300

    pratica

--- Aula01/pratica_aula01/criarBD.py
+++ Aula01/pratica_aula01/criarBD.py
@@ -1,6 +1,13 @@
-import sqlite3
+# criação do banco de dados
+# informa o banco dedados a ser utilizado através do import
+
+import sqlite3 
+
+# Criação do banco de dados
 conexao = sqlite3.connect("ctmombaça.db")
 
+# Criação do cursor (Objetos utilizados para enviar comados e receber resultados do banco de dados)
 cursor = conexao.cursor()
 
-conexao.close()
+cursor.close()   # Fechando o cursor
+conexao.close()  # Fechando a conexão
