5ac01cc8600846a8a72baf186b089424410b6368
marceloamaro <marcelo.amaro@aluno.uece.br>
2021-06-12 15:04:09 -0300

    aula

---
+++ Aula02/criar_tabela.py
@@ -0,0 +1,21 @@
+import sqlite3
+from contextlib import closing
+
+dados = [
+    ("João","111-111"),
+    ("Edinara","222-222"),
+    ("Jonas","333-333"),
+    ("Raquel","444-444")
+]
+
+with sqlite3.connect("agenda2.db") as conexao:
+    with closing(conexao.cursor()) as cursor:
+       cursor.execute(
+             """
+             create table agenda2
+             (
+                 nome text,
+                 telefone text
+             )
+             """
+         ))
