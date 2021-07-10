9824bede3aac7d2b042cfcb1619c17ba1111ce07
marceloamaro <marcelo.amaro@aluno.uece.br>
2021-07-10 11:05:13 -0300

    projeto

--- Projeto/peixaria.py
+++ Projeto/peixaria.py
@@ -3,21 +3,23 @@ from PyQt5 import  uic,QtWidgets
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout
 from PyQt5.QtCore import Qt
 import sqlite3
-
-
-def sair():
-    primeira.close()
-    segunda.close()
-    terceira.close()
-    quarta.close()
-    quinta.close()
-    sexta.close()
     
 def chama_segunda():
     primeira.label_4.setText("")
     nome_usuario = primeira.lineEdit.text()
     senha = primeira.lineEdit_2.text()
-    if nome_usuario == "marcelo123" and senha == "123456" :
+    banco = sqlite3.connect('banco_cadastro.db') 
+    cursor = banco.cursor()
+    
+    try:
+        cursor.execute("select senha from cadastro where login ='{}'".format(nome_usuario) )
+        senha_bd = cursor.fetchall()
+        print(senha_bd[0][0])
+        banco.close() 
+    except:
+        print("ERRO ao validar o login") 
+ 
+    if senha == senha_bd[0][0]:
         primeira.close()
         segunda.show()
     else :
@@ -30,6 +32,9 @@ def volta_tela():
     primeira.close()
     quinta.close()
     sexta.close()
+    sete.close()
+    oito.close()
+    tela_cadastro.close()
 
 def abre_tela_terceira():
     segunda.close()
@@ -84,37 +89,68 @@ def apagar():
     quinta.lineEdit_2.setText("")
     banco.close()
 
-
 def busca_simples():
+    oito.show()
+    quinta.close()
     busca1 = quinta.lineEdit_2.text()
     banco = sqlite3.connect('estoque.db') 
     cursor = banco.cursor()
     cursor.execute("select * from produtos where descricao = ?", (busca1,))
-    while True:
-        resultado = cursor.fetchone()
-        if resultado is None:
-            break
-        print(f"Codigo: {resultado[0]}\ndescriçao: {resultado[1],}\npreço: {resultado[2]}\ncategoria: {resultado[3]}")
+    resultado = cursor.fetchone()
+    oito.tableWidget.setRowCount(len(resultado))
+    oito.tableWidget.setColumnCount(4)
+
+    for i in range(0, len(resultado)):
+        for j in range(0, 4):
+            oito.tableWidget.setItem(i, j, QtWidgets.QTableWidgetItem(str(resultado[i][j])))
     
     quinta.lineEdit_2.setText("")
     banco.close()
 
-def busca_completa():
+def busca_completa(): 
+    sete.show()
+    quarta.close()
     banco = sqlite3.connect('estoque.db') 
     cursor = banco.cursor()
-    cursor.execute("select *from produtos")
-    
+    cursor.execute("select * from produtos")
     resultados = cursor.fetchall()
-    print("Total de arquivos:  ", len(resultados))
-    for row in resultados:
-            print("Codigo: ", row[0])
-            print("Descrição: ", row[1])
-            print("Preço: ", row[2])
-            print("Catergoria: ", row[3])
-            print("\n")
+    sete.tableWidget.setRowCount(len(resultados))
+    sete.tableWidget.setColumnCount(4)
 
-    banco.close()    
+    for i in range(0, len(resultados)):
+        for j in range(0, 4):
+            sete.tableWidget.setItem(i, j, QtWidgets.QTableWidgetItem(str(resultados[i][j])))
+    banco.close()   
+    
+def abre_tela_cadastro():
+    segunda.close()
+    tela_cadastro.show()
 
+def cadastrar():
+    nome = tela_cadastro.lineEdit.text()
+    login = tela_cadastro.lineEdit_2.text()
+    senha = tela_cadastro.lineEdit_3.text()
+    c_senha = tela_cadastro.lineEdit_4.text()
+
+    if (senha == c_senha):
+        try:
+            banco = sqlite3.connect('banco_cadastro.db') 
+            cursor = banco.cursor()
+            cursor.execute("CREATE TABLE IF NOT EXISTS cadastro (nome text,login text,senha text)")
+            cursor.execute("INSERT INTO cadastro VALUES ('"+nome+"','"+login+"','"+senha+"')")
+
+            banco.commit() 
+            tela_cadastro.lineEdit.setText("")
+            tela_cadastro.lineEdit_2.setText("")
+            tela_cadastro.lineEdit_3.setText("")
+            tela_cadastro.lineEdit_4.setText("")
+            banco.close()
+            tela_cadastro.label.setText("Usuario cadastrado com sucesso")
+
+        except sqlite3.Error as erro:
+            print("Erro ao inserir os dados: ",erro)
+    else:
+        tela_cadastro.label.setText("As senhas digitadas estão diferentes")
 
 
 app=QtWidgets.QApplication([])
@@ -126,24 +162,27 @@ quinta = uic.loadUi("quinta.ui")
 sexta = uic.loadUi("sexta.ui")
 sete = uic.loadUi("sete_quarta.ui")
 oito = uic.loadUi("oito_quinta.ui")
+tela_cadastro = uic.loadUi("tela_cadastro.ui")
 primeira.pushButton.clicked.connect(chama_segunda)
 primeira.lineEdit_2.setEchoMode(QtWidgets.QLineEdit.Password)
 segunda.pushButton_3.clicked.connect(abre_tela_terceira)
 terceira.pushButton.clicked.connect(chama_terceira)  
-segunda.pushButton_5.clicked.connect(sair)
-quarta.pushButton_5.clicked.connect(sair)
+segunda.pushButton_5.clicked.connect(abre_tela_cadastro)
+tela_cadastro.pushButton.clicked.connect(cadastrar) 
 quarta.pushButton_6.clicked.connect(volta_tela)
 segunda.pushButton.clicked.connect(abre_tela_quarta)
 terceira.pushButton_2.clicked.connect(volta_tela)
-quinta.pushButton_5.clicked.connect(sair)
 quinta.pushButton_6.clicked.connect(volta_tela)
 segunda.pushButton_2.clicked.connect(abre_tela_quinta)
-sexta.pushButton_5.clicked.connect(sair)
 sexta.pushButton_6.clicked.connect(volta_tela)
 segunda.pushButton_4.clicked.connect(abre_tela_sexta)
 quinta.pushButton_7.clicked.connect(busca_simples)
 sexta.pushButton_8.clicked.connect(apagar)
 quarta.pushButton_7.clicked.connect(busca_completa)
+sete.pushButton_6.clicked.connect(volta_tela)
+oito.pushButton_6.clicked.connect(volta_tela)
+tela_cadastro.pushButton_2.clicked.connect(volta_tela)
+
 
 
 
--- Tela de login Python/tela_login/login.py
+++ Tela de login Python/tela_login/login.py
@@ -6,11 +6,23 @@ def chama_segunda_tela():
     primeira_tela.label_4.setText("")
     nome_usuario = primeira_tela.lineEdit.text()
     senha = primeira_tela.lineEdit_2.text()
-    if nome_usuario == "joao123" and senha == "123456" :
+    banco = sqlite3.connect('banco_cadastro.db') 
+    cursor = banco.cursor()
+    
+    try:
+        cursor.execute("select senha from cadastro where login ='{}'".format(nome_usuario) )
+        senha_bd = cursor.fetchall()
+        print(senha_bd[0][0])
+        banco.close() 
+    except:
+        print("ERRO ao validar o login") 
+ 
+    if senha == senha_bd[0][0]:
         primeira_tela.close()
         segunda_tela.show()
     else :
-        primeira_tela.label_4.setText("Dados de login incorretos!")
+        primeira_tela.label_4.setText("Dados de login incorretos!") 
+
     
 
 def logout():
@@ -18,6 +30,7 @@ def logout():
     primeira_tela.show()
 
 def abre_tela_cadastro():
+    segunda_tela.close()
     tela_cadastro.show()
 
 
@@ -35,6 +48,10 @@ def cadastrar():
             cursor.execute("INSERT INTO cadastro VALUES ('"+nome+"','"+login+"','"+senha+"')")
 
             banco.commit() 
+            tela_cadastro.lineEdit.setText("")
+            tela_cadastro.lineEdit_2.setText("")
+            tela_cadastro.lineEdit_3.setText("")
+            tela_cadastro.lineEdit_4.setText("")
             banco.close()
             tela_cadastro.label.setText("Usuario cadastrado com sucesso")
 
